cf0a38d287cda82323baf4197d9db44e0829cd6d
Celine Feng <83599921+celineyayifeng@users.noreply.github.com>
2025-04-03 12:54:32 -0400

    Update objects.sql

--- bigquery_ext_tables/objects.sql
+++ bigquery_ext_tables/objects.sql
@@ -8,16 +8,6 @@ OPTIONS (
   uris = ['gs://national-gallery-art-analytics-data-bucket/objects.csv']
 );
 
-CREATE OR REPLACE EXTERNAL TABLE `art_analytics_bigquery.objects`
-OPTIONS (
-  format = 'CSV',
-  field_delimiter = ',',  -- Specify the delimiter explicitly
-  quote = '"',            -- Specify the quote character
-  allow_quoted_newlines = true,  -- Allow newlines within quoted fields
-  skip_leading_rows = 1,  -- Skip header row if present
-  uris = ['gs://national-gallery-art-analytics-data-bucket/objects_constituents.csv']
-);
-
 CREATE OR REPLACE EXTERNAL TABLE `art_analytics_bigquery.objects_constituents`
 OPTIONS (
   format = 'CSV',
