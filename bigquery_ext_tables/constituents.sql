42fd312b2998e8ae07a783ec2abd05713d0a0ebf
Celine Feng <83599921+celineyayifeng@users.noreply.github.com>
2025-04-03 21:53:09 -0400

    Update constituents.sql

--- bigquery_ext_tables/constituents.sql
+++ bigquery_ext_tables/constituents.sql
@@ -1,10 +1,26 @@
 CREATE OR REPLACE EXTERNAL TABLE `art_analytics_bigquery.constituents`
+  (
+  constituentid INT64,
+  ulanid STRING,
+  preferreddisplayname STRING,
+  forwarddisplayname STRING,
+  lastname STRING,
+  displaydate STRING,
+  artistofngaobject INT64,
+  beginyear INT64,
+  endyear INT64,
+  visualbrowsertimespan STRING,
+  nationality STRING,
+  visualbrowsernationality STRING,
+  constituenttype STRING,
+  wikidataid STRING
+)
 OPTIONS (
   format = 'CSV',
-  field_delimiter = ',',  -- Specify the delimiter explicitly
-  quote = '"',            -- Specify the quote character
-  allow_quoted_newlines = true,  -- Allow newlines within quoted fields
-  skip_leading_rows = 1,  -- Skip header row if present
+  field_delimiter = ',',           -- Explicit delimiter
+  quote = '"',                     -- Quoting character
+  allow_quoted_newlines = TRUE,   -- Support multiline fields
+  skip_leading_rows = 1,          -- Skip header
   uris = ['gs://national-gallery-art-analytics-data-bucket/constituents.csv']
 );
 
